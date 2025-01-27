8446ba442814fc2fdac0e2cc59e820f6ed4055b6
Arjun Suresh <arjunsuresh410@gmail.com>
2025-01-27 10:56:22 +0530

    new

--- BackEnd/src/index.js
+++ BackEnd/src/index.js
@@ -26,13 +26,12 @@ module.exports = app;
 // const express = require('express');
 // const connectDB = require('./config/db');
 // const menuRoutes = require('./routes/menuRoutes');
-// const foodRoutes = require("./routes/foodRouters")
-// const cors = require("cors")
+// const foodRoutes = require('./routes/foodRouters');
+// const cors = require('cors');
 
 // const app = express();
 
 // // Connect to database
-
 // connectDB();
 
 // // Middleware
@@ -43,29 +42,5 @@ module.exports = app;
 // app.use('/api/menu', menuRoutes);
 // app.use('/api/food', foodRoutes);
 
-
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
+// // Export the app for Vercel
 // module.exports = app;
-const express = require('express');
-const connectDB = require('./config/db');
-const menuRoutes = require('./routes/menuRoutes');
-const foodRoutes = require('./routes/foodRouters');
-const cors = require('cors');
-
-const app = express();
-
-// Connect to database
-connectDB();
-
-// Middleware
-app.use(express.json());
-app.use(cors());
-
-// Routes
-app.use('/api/menu', menuRoutes);
-app.use('/api/food', foodRoutes);
-
-// Export the app for Vercel
-module.exports = app;
