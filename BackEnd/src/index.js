aca1c8622259d72bc59b4abbeedb7b34dc7bc20c
Arjun Suresh <arjunsuresh410@gmail.com>
2025-01-27 10:40:05 +0530

    Update index.js

--- BackEnd/src/index.js
+++ BackEnd/src/index.js
@@ -1,37 +1,13 @@
-const express = require('express');
-const connectDB = require('./config/db');
-const menuRoutes = require('./routes/menuRoutes');
-const foodRoutes = require("./routes/foodRouters")
-const cors = require("cors")
-
-const app = express();
-
-// Connect to database
-
-connectDB();
-
-// Middleware
-app.use(express.json());
-app.use(cors());
-
-// Routes
-app.use('/api/menu', menuRoutes);
-app.use('/api/food', foodRoutes);
-
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-module.exports = app;
 // const express = require('express');
 // const connectDB = require('./config/db');
 // const menuRoutes = require('./routes/menuRoutes');
-// const foodRoutes = require('./routes/foodRouters');
-// const cors = require('cors');
+// const foodRoutes = require("./routes/foodRouters")
+// const cors = require("cors")
 
 // const app = express();
 
 // // Connect to database
+
 // connectDB();
 
 // // Middleware
@@ -42,5 +18,29 @@ module.exports = app;
 // app.use('/api/menu', menuRoutes);
 // app.use('/api/food', foodRoutes);
 
-// // Export the app for Vercel
+
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 // module.exports = app;
+const express = require('express');
+const connectDB = require('./config/db');
+const menuRoutes = require('./routes/menuRoutes');
+const foodRoutes = require('./routes/foodRouters');
+const cors = require('cors');
+
+const app = express();
+
+// Connect to database
+connectDB();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Routes
+app.use('/api/menu', menuRoutes);
+app.use('/api/food', foodRoutes);
+
+// Export the app for Vercel
+module.exports = app;
